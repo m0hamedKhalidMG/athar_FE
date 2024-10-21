1f4d2d91865d218881f5493e1a5a5f2e69cf57ac
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2024-10-22 01:57:52 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -157,7 +157,7 @@ const Footer = () => {
       <WhatsAppContainer>
         <FontAwesomeIcon icon={faWhatsapp} />
         <a
-  href="https://wa.me/201004395179"
+  href="https://wa.me/phone"
   target="_blank"
   rel="noopener noreferrer"
 >
